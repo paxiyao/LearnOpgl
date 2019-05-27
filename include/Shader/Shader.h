8b59e87766d89b9c979f94ea945ed9e593c032df
paxiyao <249003697@qq.com>
2019-05-27 22:03:58 +0800

    coordinateSystem

--- include/Shader/Shader.h
+++ include/Shader/Shader.h
@@ -8,6 +8,9 @@
 #include "fstream"
 #include "sstream"
 #include "iostream"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader
 {
@@ -26,6 +29,7 @@ public:
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
+	void setMat4(const std::string& name, glm::mat4& mat) const;
 private:
 	char infoLog[INFOLEN];
 };
