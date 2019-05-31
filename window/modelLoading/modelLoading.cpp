7b6e6580aa00a3cbec556af26fd24f9bffabe522
paxiyao <249003697@qq.com>
2019-05-31 16:36:06 +0800

    model

--- window/modelLoading/modelLoading.cpp
+++ window/modelLoading/modelLoading.cpp
@@ -74,12 +74,13 @@ int main()
 
 	// build and compile shaders
 	// -------------------------
-	Shader ourShader("1.model_loading.vs", "1.model_loading.fs");
-
+	Shader ourShader1("1.model_loading.vs", "1.model_loading.fs");
 	// load models
 	// -----------
-	Model ourModel("resources/objects/nanosuit/nanosuit.obj");
+	Model ourModel1("resources/objects/nanosuit/nanosuit.obj");
 
+	Shader ourShader2("1.model_loading.vs", "1.model_loading.fs");
+	Model ourModel2("resources/objects/nanosuit/nanosuit.obj");
 
 	// draw in wireframe
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -104,21 +105,31 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// don't forget to enable shader before setting uniforms
-		ourShader.use();
+		ourShader1.use();
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
-		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
+		ourShader1.setMat4("projection", projection);
+		ourShader1.setMat4("view", view);
 
 		// render the loaded model
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::translate(model, glm::vec3(-1.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
 		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
-		ourShader.setMat4("model", model);
-		ourModel.Draw(ourShader);
+		ourShader1.setMat4("model", model);
+		ourModel1.Draw(ourShader1);
 
+		// don't forget to enable shader before setting uniforms
+		ourShader2.use();
+		ourShader2.setMat4("projection", projection);
+		ourShader2.setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(1.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		ourShader2.setMat4("model", model);
+		ourModel2.Draw(ourShader2);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
