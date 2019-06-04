100a9ce4a3ddcf52e20a9430cfe184c4def3462d
paxiyao <249003697@qq.com>
2019-06-04 23:31:23 +0800

    blur

--- window/frameBuffers/1.1.screen_fragment.glsl
+++ window/frameBuffers/1.1.screen_fragment.glsl
@@ -17,8 +17,68 @@ void main()
 //    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
 //    FragColor = vec4(average, average, average, 1.0);
 
-	// Kernel effects
-	 vec2 offsets[9] = vec2[](
+//	// Kernel effects
+//	 vec2 offsets[9] = vec2[](
+//        vec2(-offset,  offset), // 左上
+//        vec2( 0.0f,    offset), // 正上
+//        vec2( offset,  offset), // 右上
+//        vec2(-offset,  0.0f),   // 左
+//        vec2( 0.0f,    0.0f),   // 中
+//        vec2( offset,  0.0f),   // 右
+//        vec2(-offset, -offset), // 左下
+//        vec2( 0.0f,   -offset), // 正下
+//        vec2( offset, -offset)  // 右下
+//    );
+//
+//    float kernel[9] = float[](
+//        -1, -1, -1,
+//        -1,  9, -1,
+//        -1, -1, -1
+//    );
+//
+//    vec3 sampleTex[9];
+//    for(int i = 0; i < 9; i++)
+//    {
+//        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+//    }
+//    vec3 col = vec3(0.0);
+//    for(int i = 0; i < 9; i++)
+//        col += sampleTex[i] * kernel[i];
+//
+//    FragColor = vec4(col, 1.0);
+
+//	// Blur effects
+//	vec2 offsets[9] = vec2[](
+//        vec2(-offset,  offset), // 左上
+//        vec2( 0.0f,    offset), // 正上
+//        vec2( offset,  offset), // 右上
+//        vec2(-offset,  0.0f),   // 左
+//        vec2( 0.0f,    0.0f),   // 中
+//        vec2( offset,  0.0f),   // 右
+//        vec2(-offset, -offset), // 左下
+//        vec2( 0.0f,   -offset), // 正下
+//        vec2( offset, -offset)  // 右下
+//    );
+//
+//	float kernel[9] = float[](
+//		1, 1, 1,
+//		1, 4.0 / 16, 1,
+//		1, 1, 1
+//	);
+//
+//    vec3 sampleTex[9];
+//    for(int i = 0; i < 9; i++)
+//    {
+//        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+//    }
+//    vec3 col = vec3(0.0);
+//    for(int i = 0; i < 9; i++)
+//        col += sampleTex[i] * kernel[i];
+//
+//    FragColor = vec4(col, 1.0);
+
+	// Edge-detection
+	vec2 offsets[9] = vec2[](
         vec2(-offset,  offset), // 左上
         vec2( 0.0f,    offset), // 正上
         vec2( offset,  offset), // 右上
@@ -30,11 +90,11 @@ void main()
         vec2( offset, -offset)  // 右下
     );
 
-    float kernel[9] = float[](
-        -1, -1, -1,
-        -1,  9, -1,
-        -1, -1, -1
-    );
+	float kernel[9] = float[](
+		1, 1, 1,
+		1, -8, 1,
+		1, 1, 1
+	);
 
     vec3 sampleTex[9];
     for(int i = 0; i < 9; i++)
@@ -46,5 +106,6 @@ void main()
         col += sampleTex[i] * kernel[i];
 
     FragColor = vec4(col, 1.0);
+
 //    FragColor = texture(screenTexture, TexCoords);
 }
