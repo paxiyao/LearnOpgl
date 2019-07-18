4c46e4736603f979ad72ca6092073fe863ae160c
paxiyao <249003697@qq.com>
2019-07-18 18:06:47 +0800

    add particles

--- Breakout/Breakout/game.h
+++ Breakout/Breakout/game.h
@@ -8,6 +8,8 @@
 ******************************************************************/
 #ifndef GAME_H
 #define GAME_H
+#include <vector>
+#include <tuple>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -21,11 +23,21 @@ enum GameState {
     GAME_WIN
 };
 
+// Represents the four possible (collision) directions
+enum Direction {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+};
+
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<GLboolean, Direction, glm::vec2> Collision;
+
 // Initial size of the player paddle
 const glm::vec2 PLAYER_SIZE(100, 20);
 // Initial velocity of the player paddle
 const GLfloat PLAYER_VELOCITY(500.0f);
-
 // Initial velocity of the ball
 const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
 // Initial radius of the ball
@@ -55,6 +67,10 @@ public:
     void ProcessInput(GLfloat dt);
     void Update(GLfloat dt);
     void Render();
+	// Reset level
+	void ResetLevel();
+	// Reset Player
+	void ResetPlayer();
 };
 
 #endif
